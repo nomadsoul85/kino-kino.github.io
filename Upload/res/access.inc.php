90e1c7cff2c12b2f208c396848235dbc61d12cb7
nomadsoul85 <kostyapopov76@gmail.com>
2016-07-21 22:47:35 +0300

    Initial commit
    kino

---
+++ Upload/res/access.inc.php
@@ -0,0 +1,30 @@
+<?php
+
+// Users data
+$imSettings['access']['webregistrations_gid'] = 's976g833';
+$imSettings['access']['users'] = array(
+	'admin' => array(
+		'groups' => array('6i7duqga'),
+		'id' => '6i7duqga',
+		'name' => 'Admin',
+		'password' => 'y4769e3b',
+		'email' => '',
+		'page' => 'index.html'
+	),
+	'новый пользователь' => array(
+		'groups' => array('is6ds1pz'),
+		'id' => 'is6ds1pz',
+		'name' => 'Новый пользователь',
+		'password' => 'w66gh0fh',
+		'email' => '',
+		'page' => 'index.html'
+	)
+);
+
+// Admins list
+$imSettings['access']['admins'] = array('6i7duqga');
+
+// Page/Users permissions
+$imSettings['access']['pages'] = array();
+
+// End of file access.inc.php
---
+++ Upload/res/x5settings.js
@@ -0,0 +1,112 @@
+(function ( $, x5engine ) {
+	var x = x5engine,
+		s = x.settings,
+		p = s.currentPath,
+		b = x.boot;
+
+	s.siteId = '57792AF0CAFDA665E540D6D4A7B247DF';
+	s.version = '12-0-6-24';
+	b.push(function () {
+		x.setupDateTime();
+		x.imAccess.showLogout();
+		x.utils.autoHeight();
+		x.cart.ui.updateWidget();
+		x.imGrid.init();
+	});
+	s.menu = {
+		verticalScroll: false,
+		orientation: 'horizontal'
+	};
+	b.push(function () {
+		x.menu({
+			target: '#imMnMn',
+			showCurrent: false,
+			desktopVerticalScroll: false,
+			mobileVerticalScroll: false,
+			showLogoOnScroll: false,
+			orientation: 'horizontal',
+			menuHeight: 29,
+			menuWidth: 120,
+			submenuHeight: 25,
+			submenuWidth: 160,
+			opacity: 0.8,
+			type: 'singleColumn',
+			alignment: 'left',
+			effect: 'fade'
+		});
+	});
+	b.push(function () { x.utils.imPreloadImages([p + 'menu/hor_main.png',p + 'menu/hor_main_h.png',p + 'menu/hor_main_c.png',p + 'menu/ver_main.png',p + 'menu/ver_main_h.png',p + 'menu/ver_main_c.png', p + 'res/imLoad.gif', p + 'res/imClose.png']); });
+
+	// ShowBox
+	$.extend(s.imShowBox, {
+		'effect' : 'fade',
+		'shadow' : '',
+		'background' : '#000000',
+		'borderWidth' : {
+			'top': 1,
+			'right': 1,
+			'bottom': 1,
+			'left': 1
+		},
+		buttonRight: {
+			url: p + 'res/b01_r.png',
+			position: {
+				x: -12,
+				y: 0
+			}
+		},
+		buttonLeft: {
+			url: p + 'res/b01_l.png',
+			position: {
+				x: -12,
+				y: 0
+			}
+		},
+		'borderRadius' : '5px 5px 5px 5px',
+		'borderColor' : '#000000 #000000 #000000 #000000',
+		'textColor' : '#000000',
+		'fontFamily' : 'Tahoma',
+		'fontStyle' : 'normal',
+		'fontWeight' : 'normal',
+		'fontSize' : '9pt',
+		'textAlignment' : 'left',
+		'boxColor' : '#FFFFFF',
+		'opacity' : 0.7,
+		'radialBg' : true // Works only in Mozilla Firefox and Google Chrome
+	});
+
+	// PopUp
+	$.extend(s.imPopUp, {
+		'effect' : 'fade',
+		'width' : 500,
+		'shadow' : '',
+		'background' : '#000000',
+		'borderRadius' : 10,
+		'textColor' : '#000000',
+		'boxColor' : '#FFFFFF',
+		'opacity' : 0.7
+	});
+
+	// Tip
+	$.extend(s.imTip, {
+		'borderRadius' : 1,
+		'arrow' : true,
+		'position' : 'bottom',
+		'effect' : 'none',
+		'showTail' : true
+	});
+
+	// Captcha
+	b.push(function () {
+		x5engine.captcha.instance = new x5engine.captcha.x5captcha({
+			"phpfile": "captcha/x5captcha.php",
+		});
+	}, false, 'first');
+
+	// BreakPoints
+	s.breakPoints.push({ "hash": "fc23f27c1080aa74e1efeec9f2399630", "name": "Desktop", "start": "max", "end": 0, "fluid": false });
+
+	b.push(function () { x.cookielaw.activateScripts() });
+
+	s.loaded = true;
+})( _jq, x5engine );
